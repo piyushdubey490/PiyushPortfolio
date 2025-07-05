b6d2fd5d5c7fa36d238f42ec27ed44f39ae93c10
Piyush Dubey <pd2000yp@gmail.com>
2025-07-05 18:12:09 +0530

    Add initial migration files for contact_messages and users tables

---
+++ migrations/0000_wooden_tyrannus.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "contact_messages" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"subject" text NOT NULL,
+	"budget" text,
+	"message" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"username" text NOT NULL,
+	"password" text NOT NULL,
+	CONSTRAINT "users_username_unique" UNIQUE("username")
+);
